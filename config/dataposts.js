5d310302cddb85f79d41c6ded84abee119febf42
claiunocode <claiunocode@gmail.com>
2024-10-19 14:46:18 -0500

    add another post

--- config/dataposts.js
+++ config/dataposts.js
@@ -2,18 +2,11 @@ const dataToPosts = {
     "Automatizacion":
         [
             {
-                "title":"Gestion de servicios Ubuntu",
-                "description":"Script en Bash que gestiona los servicios disponibles en una distribución de Ubuntu",
-                "date":"02/02/2024",
-                "id":"1",
-                "img":"https://assets.ubuntu.com/v1/82818827-CoF_white.svg"
-            },
-            {
-                "title":"Gestion de servicios Ubuntu",
-                "description":"Script en Bash que gestiona los servicios disponibles en una distribución de Ubuntu",
-                "date":"02/02/2024",
-                "id":"1",
-                "img":"https://assets.ubuntu.com/v1/82818827-CoF_white.svg"
+                "title": "Scraping y Verificación de Errores",
+                "description": "Script en Bash y Python que verifica enlaces y errores en una página web.",
+                "date": "02/02/2024",
+                "id": "1",
+                "img": "https://pypi.org/static/images/logo-small.8998e9d1.svg" // Placeholder for the logo
             },
             
         ],
@@ -27,7 +20,7 @@ const dataToPosts = {
         },
         {
             "title": "Monitor de Recursos del Sistema",
-            "description": "Script en Bash que muestra el uso de CPU, RAM y disco duro de manera visual.",
+            "description": "Script en Bash que muestra el uso de CPU, RAM y disco duro",
             "date": "02/02/2024",
             "id": "2",
             "img": "https://cdn1.iconfinder.com/data/icons/computer-hardware-532/64/Processor-chip-cpu-memory-ram-512.png" // Placeholder for the logo

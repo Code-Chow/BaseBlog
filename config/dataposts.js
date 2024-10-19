ae339a51312e53d865d4c15238c8ae62741b22bd
claiunocode <claiunocode@gmail.com>
2024-10-19 14:36:58 -0500

    add another post

--- config/dataposts.js
+++ config/dataposts.js
@@ -25,6 +25,22 @@ const dataToPosts = {
             "id":"1",
             "img":"https://assets.ubuntu.com/v1/82818827-CoF_white.svg"
         },
+        {
+            "title": "Monitor de Recursos del Sistema",
+            "description": "Script en Bash que muestra el uso de CPU, RAM y disco duro de manera visual.",
+            "date": "02/02/2024",
+            "id": "2",
+            "img": "https://cdn1.iconfinder.com/data/icons/computer-hardware-532/64/Processor-chip-cpu-memory-ram-512.png" // Placeholder for the logo
+        }
+    ],
+    "InteligenciaArtificial":[
+        {
+            "title":"Consulta a BITO Chat",
+            "description":"Consulta al chat de IA de Bito, tienes varias opciones con este scrpt",
+            "date":"02/02/2024",
+            "id":"1",
+            "img":"https://bito.ai/wp-content/uploads/2023/10/Logo-Bito-Black-cropped.svg"
+        },
     ],
     "Utilidades":[
         {
